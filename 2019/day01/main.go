fd822959a39a6daf4dca95f311091f1a87515121
Ramy Aboul Naga <ramy.naga@gmail.com>
2020-03-18 19:09:00 +0100

    day 01 part 01

---
+++ 2019/day01/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var totalFuel int
+
+	sc := bufio.NewScanner(os.Stdin)
+	for i := 0; sc.Scan(); i++ {
+		line := sc.Text()
+		mass, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatalf("Failed to parse line %d %s : %v", i, line, err)
+		}
+		fuel := calcFuel(mass)
+		totalFuel += fuel
+	}
+	if err := sc.Err(); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Total fuel: %d", totalFuel)
+}
+
+func calcFuel(mass int) int {
+	return mass/3 - 2
+}
